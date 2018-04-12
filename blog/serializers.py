837032a5d7bf16625812c7800e5aa3b845219466
Yang Zhuoze <yangzhuoze@163.com>
2018-04-12 23:08:35 +0800

    generate uuid for creating article

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
 from blog.models import Article
+from common.utils.generate import uid_generate
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('uid', 'title', 'body', 'body_raw', 'head_pic', 'created_time', 'modified_time', 'views_count')
+        fields = '__all__'
+        read_only_fields = ('uid', )
+
+    def create(self, validated_data):
+        article = Article(
+            uid=uid_generate(),
+            title=validated_data['title'], 
+            body=validated_data['body'], 
+            body_raw=validated_data['body_raw'], 
+            head_pic=validated_data['head_pic'], 
+            created_time=validated_data['created_time'], 
+            modified_time=validated_data['modified_time'], 
+            views_count=validated_data['views_count'], 
+        )
+        article.save()
+        return article
---
+++ common/utils/generate.py
@@ -0,0 +1,7 @@
+import hashlib
+import uuid
+
+
+def uid_generate():
+    uid = str(uuid.uuid4())
+    return uid[:8]

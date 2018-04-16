bcf19d03b6fbefbc6e52844533a6aa8b25048c7b
Yang Zhuoze <yangzhuoze@163.com>
2018-04-16 23:15:52 +0800

    add API root

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 
 urlpatterns = [
     path('articles/', views.ArticleList.as_view()),
+    path('', views.api_root, name='article-list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import generics
+from rest_framework import generics, renderers
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from blog.models import Article
 from blog.serializers import ArticleSerializer
@@ -22,6 +25,12 @@ from blog.serializers import ArticleSerializer
 #             return JsonResponse(article.data, status=201)
 #         return JsonResponse(article.errors, status=400)
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'articles': reverse('article-list', request=request, format=format)
+    })
+
 class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
@@ -29,3 +38,11 @@ class ArticleList(generics.ListCreateAPIView):
 class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+class ArticleMarkdown(generics.GenericAPIView):
+    queryset = Article.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        article = self.get_object()
+        return Response(article.body)
